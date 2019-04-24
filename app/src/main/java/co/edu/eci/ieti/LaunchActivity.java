4fb999ccf5c401bc5fc8cfb4b68592a23bdc2a4c
sancarbar <sancarbar@gmail.com>
2019-04-23 21:59:49 -0500

    Implemented SharedPreferences Token authentication validation.

--- app/src/main/java/co/edu/eci/ieti/LaunchActivity.java
+++ app/src/main/java/co/edu/eci/ieti/LaunchActivity.java
@@ -1,6 +1,8 @@
 package co.edu.eci.ieti;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +15,24 @@ public class LaunchActivity
     extends AppCompatActivity
 {
 
+    public static final String TOKEN_KEY = "TOKEN_KEY";
+
     @Override
     protected void onCreate( @Nullable Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        startActivity( new Intent( this, MainActivity.class ) );
+        SharedPreferences sharedPref =
+            getSharedPreferences( getString( R.string.preference_file_key ), Context.MODE_PRIVATE );
+
+        if ( sharedPref.contains( TOKEN_KEY ) )
+        {
+            startActivity( new Intent( this, MainActivity.class ) );
+        }
+        else
+        {
+            startActivity( new Intent( this, LoginActivity.class ) );
+        }
+
+
     }
 }
